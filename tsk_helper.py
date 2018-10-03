5c3c5aa83fd7d91d649aa3aa394e10b97ca064c8
MustyMouse <admin@krux.us>
2018-10-03 10:50:22 -0500

    Update tsk_helper.py

--- tsk_helper.py
+++ tsk_helper.py
@@ -1,10 +1,8 @@
 import sys
 
-
 def get_fsstat_output(f):
     """
     Returns assignment-defined output (for unit tests) from `fsstat` output.
-
 
     :param f: open file-like object containing `fsstat` output
     :return: list of strings representing unit-testable output
@@ -32,11 +30,9 @@ def get_fsstat_output(f):
         line = f.readline()
     return result
 
-
 def strip_all(lines):
     """Returns a copy of the list of lines where all lines have been strip()ped."""
     return [line.strip() for line in lines]
-
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
